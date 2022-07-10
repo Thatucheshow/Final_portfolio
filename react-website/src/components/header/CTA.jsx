16b10fd8258eddb1a0c085d620a270a0ad25c96d
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-10 17:31:29 -0400

    First Profile Photo added

---
+++ react-website/src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href='' className='btn'>Download Resume</a>
+        <a href='#contact' className='btn-primary'>Let's Chat</a>
+        
+    </div>
+  )
+}
+
+export default CTA
--- react-website/src/components/header/Header.jsx
+++ react-website/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.JPG'
 
 const Header = () => {
   return (
@@ -7,7 +9,13 @@ const Header = () => {
       <div className="container header_container">
         <h5> Hello I'm</h5>
         <h1>Uchechukwu Okeke</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>A Software Engineer with a background in healthcare and production. I have a passion for writing problem solving codes. I am excited to work with a team of engineers who prioritize efficiency and growth while staying updated on the latest trends and technologies. 
+        </h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
       </div>
     </header>
   )
