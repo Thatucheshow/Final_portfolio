ad56fd3a8fc18b66927c40820513b93503ba9f81
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-11 23:48:56 -0400

    Nav icons done

--- react-website/src/components/nav/Nav.jsx
+++ react-website/src/components/nav/Nav.jsx
@@ -4,15 +4,18 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {AiOutlineMessage} from 'react-icons/ai'
+import {useState} from 'react'
 
 
-function Nav() {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState ('#')
+
   return (
    <nav>
-     <a href="#"><AiOutlineHome/></a>
-     <a href="#about"><AiOutlineUser/></a>
-     <a href="#portfolio"><BiBook/></a>
-     <a href="#contact"><AiOutlineMessage/></a>
+     <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+     <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+     <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiBook/></a>
+     <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
    </nav>
   )
 }
