d5f0c5897196cbd261c5a1b88897ea81c74dd5d7
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-12 12:46:43 -0400

    working on portfolio

--- react-website/src/components/portfolio/Portfolio.jsx
+++ react-website/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/realitive.jpg'
+import IMG2 from '../../assets/sleepApp.jpg'
 
 function Portfolio() {
   return (
@@ -8,11 +10,33 @@ function Portfolio() {
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <div className='portfolio__item-image'></div>
-          <h3>title</h3>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Project Image" />
+          </div>
+          <h3>Realitive</h3>
           <a href="https://github.com/sean-r-g/realestate-back-end" className='btn'>Github Backend</a>
           <a href="https://github.com/sean-r-g/realestate-front-end" className='btn'>Github Frontend</a>
-          <a href="https://real-estate-front-end12.herokuapp.com/#" className=''>Live Demo</a>
+          <a href="https://real-estate-front-end12.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt="Project Image" />
+          </div>
+          <h3>Sleep Tracker</h3>
+          <a href="https://github.com/kaibrown28/sleepapp-backend" className='btn'>Github Backend</a>
+          <a href="https://github.com/caseyoneil10/sleep_front_end" className='btn'>Github Frontend</a>
+          <a href="https://protected-hamlet-59115.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Project Image" />
+          </div>
+          <h3>Realitive</h3>
+          <a href="https://github.com/sean-r-g/realestate-back-end" className='btn'>Github Backend</a>
+          <a href="https://github.com/sean-r-g/realestate-front-end" className='btn'>Github Frontend</a>
+          <a href="https://real-estate-front-end12.herokuapp.com/#" className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
       </div>
     </section>
