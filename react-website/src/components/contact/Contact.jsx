cc64b2f2d780352ab1c7293e33d41817ef8c662f
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-12 14:22:53 -0400

    working on contact form

--- react-website/src/components/contact/Contact.jsx
+++ react-website/src/components/contact/Contact.jsx
@@ -4,7 +4,18 @@ import './contact.css'
 const contact = () => {
   return (
     <section id='contact'>
-      contact
+      <h5>Get In Touch</h5>
+      <h2>Drop Me A Line</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <h4>Email</h4>
+            <h5>robinsonokeke60@gmail.com</h5>
+          </article>
+        </div>
+      </div>
+      
     </section>
   )
 }
