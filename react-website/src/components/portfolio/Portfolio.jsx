e487949e4992c2a8016a20df56cd28bf2f25c142
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-12 14:14:27 -0400

    portfolio done

--- react-website/src/components/portfolio/Portfolio.jsx
+++ react-website/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,9 @@ import './portfolio.css'
 import IMG1 from '../../assets/realitive.jpg'
 import IMG2 from '../../assets/sleepApp.jpg'
 
-function Portfolio() {
+const Portfolio = () => {
+
+
   return (
     <section id='portfolio'>
       <h2>Some Things I've Built</h2>
@@ -14,9 +16,11 @@ function Portfolio() {
             <img src={IMG1} alt="Project Image" />
           </div>
           <h3>Realitive</h3>
-          <a href="https://github.com/sean-r-g/realestate-back-end" className='btn'>Github Backend</a>
-          <a href="https://github.com/sean-r-g/realestate-front-end" className='btn'>Github Frontend</a>
+          <div className='portfolio__item-cta'>
+          <a href="https://github.com/sean-r-g/realestate-back-end" className='btn' target='_blank'>Github Backend</a>
+          <a href="https://github.com/sean-r-g/realestate-front-end" className='btn' target='_blank'>Github Frontend</a>
           <a href="https://real-estate-front-end12.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
 
         <article className='portfolio__item'>
@@ -24,9 +28,11 @@ function Portfolio() {
             <img src={IMG2} alt="Project Image" />
           </div>
           <h3>Sleep Tracker</h3>
-          <a href="https://github.com/kaibrown28/sleepapp-backend" className='btn'>Github Backend</a>
-          <a href="https://github.com/caseyoneil10/sleep_front_end" className='btn'>Github Frontend</a>
+          <div className='portfolio__item-cta'>
+          <a href="https://github.com/kaibrown28/sleepapp-backend" className='btn' target='_blank'>Github Backend</a>
+          <a href="https://github.com/caseyoneil10/sleep_front_end" className='btn' target='_blank'>Github Frontend</a>
           <a href="https://protected-hamlet-59115.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
 
         
