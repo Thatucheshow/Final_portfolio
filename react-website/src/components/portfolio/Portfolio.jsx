144fa12dcdb3d91cd4e4aa94fac6c047c60b209c
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-12 12:11:37 -0400

    working on portfolio

--- react-website/src/components/portfolio/Portfolio.jsx
+++ react-website/src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,17 @@ import './portfolio.css'
 function Portfolio() {
   return (
     <section id='portfolio'>
-      Projects
+      <h2>Some Things I've Built</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>title</h3>
+          <a href="https://github.com/sean-r-g/realestate-back-end" className='btn'>Github Backend</a>
+          <a href="https://github.com/sean-r-g/realestate-front-end" className='btn'>Github Frontend</a>
+          <a href="https://real-estate-front-end12.herokuapp.com/#" className=''>Live Demo</a>
+        </article>
+      </div>
     </section>
   )
 }
