f5f4d68345d674dde457a6534875bf8678ba9f74
ThatUche <newuser@Uches-MacBook-Air.local>
2022-07-13 15:37:41 -0400

    Portfolio completed

--- react-website/src/components/header/CTA.jsx
+++ react-website/src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RESUME from '../../assets/Resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='' className='btn'>Download Resume</a>
+        <a href={RESUME} className='btn' target='_blank'> Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Chat</a>       
     </div>
   )
