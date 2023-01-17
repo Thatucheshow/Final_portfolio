33b81b4946a8bc7880d3db6c4054a7eda0fe1d1e
ThatUche <newuser@Uches-MacBook-Air.local>
2023-01-17 10:33:26 -0500

    typewriter update

--- react-website/src/components/header/Header.jsx
+++ react-website/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.JPG'
 import HeaderSocials from './HeaderSocials'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Header = () => {
   return (
@@ -10,8 +11,19 @@ const Header = () => {
       <div className="container header_container">
         <h5> Hello I'm</h5>
         <h1>Uchechukwu Okeke</h1>
-        <h5 className='text-light'> A Software Developer 
-        </h5>
+        <span style={{color:'darkgrey', fontWeight:'bold'}}>
+          <Typewriter 
+          words={['A Software Developer', 'Fitness Professional', 'Content Creator']}
+          loop={false}
+          cursor
+          cursorStyle='_'
+          typeSpeed={100}
+          deleteSpeed={80}
+          delaySpeed={1000}
+          />
+        </span>
+        {/* <h5 className='text-light'> A Software Developer 
+        </h5> */}
         <CTA />
         <HeaderSocials/>
 
